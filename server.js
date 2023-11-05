7e7211e132ffb912bfd7cb7d5aba33b99fd82314
Yiyun Zhang <yiz027@ucsd.edu>
2023-11-05 16:00:49 -0500

    Update favicon

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.get("/", async function (req, res) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Simple To-Do App</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link rel="icon" type="image/x-icon" href="/favicon.ico">
   </head>
   <body>
   <div class="container">
